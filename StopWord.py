a1216718b056fd3526a33afc0d2afc086228b7f5
zaher88abd <zaher88abd@gmail.com>
2018-07-29 22:17:01 -0300

    remove newline from text

--- StopWord.py
+++ StopWord.py
@@ -1,4 +1,7 @@
 def load_stop_word():
     file = "data/scikitlearn_stopword.txt"
     file = open(file)
-    return file.readlines()
+    stop_list = list()
+    for l in file.readlines():
+        stop_list.append(l.split("\n")[0])
+    return stop_list
