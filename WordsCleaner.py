284651b01c1b3d88f83003a740b6438cc0f92d1a
Zaher Abd-Ulmoula <zaher88abd@gmail.com>
2018-07-30 15:20:52 -0300

    fix bug in WordCleaner

--- WordsCleaner.py
+++ WordsCleaner.py
@@ -6,21 +6,22 @@ from pyspark.sql.types import ArrayType, StringType
 
 import re
 
+
 class WordCleaner(Transformer, HasInputCol, HasOutputCol):
     @keyword_only
-    def __init__(self,inputCol=None, outputCol=None):
+    def __init__(self, inputCol=None, outputCol=None):
         super(WordCleaner, self).__init__()
         kwargs = self._input_kwargs
         self.setParams(**kwargs)
 
     @keyword_only
-    def setParams(self, inputCol=None, outputCol=None, stopwords=None):
+    def setParams(self, inputCol=None, outputCol=None):
         kwargs = self._input_kwargs
         return self._set(**kwargs)
 
     def _transform(self, dataset):
         def f(s):
-            return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)", " ", s).split())
+            return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)", " ", s).split()).lower().split()
 
         t = ArrayType(StringType())
         out_col = self.getOutputCol()
