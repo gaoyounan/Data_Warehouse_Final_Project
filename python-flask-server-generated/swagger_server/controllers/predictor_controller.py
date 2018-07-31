3bcefb2d0ebeecab35553f67c0ccdc945bc54138
zaher88abd <zaher88abd@gmail.com>
2018-07-31 13:20:55 -0300

    add swagger server

---
+++ python-flask-server-generated/swagger_server/controllers/predictor_controller.py
@@ -0,0 +1,30 @@
+import connexion
+import six
+
+from swagger_server import util
+
+
+def get_result_tweet_id(itemid):  # noqa: E501
+    """predict the salses
+
+    Returns a single number for the salse # noqa: E501
+
+    :param itemid: ID of item to return
+    :type itemid: int
+
+    :rtype: None
+    """
+    return 'do some magic!'
+
+
+def set_tweet_id(itemid):  # noqa: E501
+    """predict the salses
+
+    Returns a single number for the salse # noqa: E501
+
+    :param itemid: ID of item to return
+    :type itemid: int
+
+    :rtype: None
+    """
+    return 'do some magic!'
