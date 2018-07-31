2325f1d10ee5556b4b6c2772ad604acf4d20a775
gaoyounan <gaoyounan@foxmail.com>
2018-07-31 11:56:56 -0300

    while loop to fetch the tweets

--- EtractTweets.py
+++ EtractTweets.py
@@ -13,6 +13,7 @@ from datetime import datetime
 from elasticsearch import Elasticsearch
 from elasticsearch.helpers import bulk
 import random
+import time
 
 def get_tweet(tweet):
     text = tweet.text
@@ -63,68 +64,136 @@ _settings = {
   }
 }
 
+def extractTweets(status_id, duration):
+
+
+        for times in range(duration):
+                consumer_key = "GG1MmGFXWbVEvjAz5thB5EQDs"
+                consumer_secret = "NG0nsSsy0Iu29RKVr2z3hSiL4HcwcHievXfE8Qw4r6x77AdPd0"
+                access_token = "1002349562093363200-O5m7LI30kIMuruS9U2tCs06zza2711"
+                access_token_secret = "i0o8KPVxU6pIRcrmzW60vmpJ1CbS6oib9IXDt28tgpqXP"
+
+                auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+                auth.secure = True
+                auth.set_access_token(access_token, access_token_secret)
+
+                # api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True, retry_count=10, retry_delay=5, retry_errors=5)
+                api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+
+                tweet = api.get_status(status_id)
+                actions = []
+                tweet_id = tweet.id
+
+                action = {
+                        "_index": "tweet_status_index",
+                        "_type": "_doc",
+                        "_id": tweet_id,
+                        "_source": generateESData(tweet)
+                }
+                actions.append(action.copy())
+
+                user = tweet.user.screen_name
+                max_id = None
+
+                query = 'to:' + user
+
+                replies = api.search(q=query, since_id=tweet_id, max_id=max_id, count=100)
+                num = 0
+                for reply in replies:
+                        if reply.in_reply_to_status_id == status_id:
+                                reply_id = reply.id
+                                replyAction = {
+                                        "_index": "tweet_status_index",
+                                        "_type": "_doc",
+                                        "_id": reply_id,
+                                        "_source": generateESData(reply)
+                                }
+
+                                actions.append(replyAction.copy())
+
+                                print reply.id
+                                print reply.created_at
+                                print reply.text
+                                num = num + 1
+
+                                # tweet = get_tweet(reply)
+                                # client_socket.send((tweet[2] + "\n").encode('utf-8'))
+                print num
+
+                es = Elasticsearch()
+                es.indices.create(index="_settings", body=_settings.copy())
+                res = bulk(es, actions)
+                es.indices.refresh(index="tweet_status_index")
+                times = times + 1
+                if times < duration:
+                        time.sleep(300)
+
+
 
 if __name__ == '__main__':
 
-        consumer_key = "GG1MmGFXWbVEvjAz5thB5EQDs"
-        consumer_secret = "NG0nsSsy0Iu29RKVr2z3hSiL4HcwcHievXfE8Qw4r6x77AdPd0"
-        access_token = "1002349562093363200-O5m7LI30kIMuruS9U2tCs06zza2711"
-        access_token_secret = "i0o8KPVxU6pIRcrmzW60vmpJ1CbS6oib9IXDt28tgpqXP"
-
-
-        auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-        auth.secure = True
-        auth.set_access_token(access_token, access_token_secret)
-
-        #api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True, retry_count=10, retry_delay=5, retry_errors=5)
-        api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
-
         status_id = 1023224248842760194
+        extractTweets(status_id , 1)
 
-        tweet = api.get_status(status_id)
-        actions = []
-        tweet_id = tweet.id
-
-        action = {
-                "_index": "tweet_status_index",
-                "_type": "_doc",
-                "_id": tweet_id,
-                "_source": generateESData(tweet)
-        }
-        actions.append(action.copy())
-
-
-        user = tweet.user.screen_name
-        max_id = None
-
-        query = 'to:'+user
-
-        replies = api.search(q=query, since_id=tweet_id, max_id=max_id, count=100)
-        num = 0
-        for reply in replies:
-            if reply.in_reply_to_status_id == status_id:
-
-                    reply_id = reply.id
-                    replyAction = {
-                            "_index": "tweet_status_index",
-                            "_type": "_doc",
-                            "_id": reply_id,
-                            "_source": generateESData(reply)
-                    }
-
-                    actions.append(replyAction.copy())
-
-                    print reply.id
-                    print reply.created_at
-                    print reply.text
-                    num = num+1
-
-                # tweet = get_tweet(reply)
-                # client_socket.send((tweet[2] + "\n").encode('utf-8'))
-        print num
-
-        es = Elasticsearch()
-        es.indices.create(index="_settings", body=_settings.copy())
-        res = bulk(es, actions)
-        es.indices.refresh(index="tweet_status_index")
+        # consumer_key = "GG1MmGFXWbVEvjAz5thB5EQDs"
+        # consumer_secret = "NG0nsSsy0Iu29RKVr2z3hSiL4HcwcHievXfE8Qw4r6x77AdPd0"
+        # access_token = "1002349562093363200-O5m7LI30kIMuruS9U2tCs06zza2711"
+        # access_token_secret = "i0o8KPVxU6pIRcrmzW60vmpJ1CbS6oib9IXDt28tgpqXP"
+        #
+        #
+        # auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+        # auth.secure = True
+        # auth.set_access_token(access_token, access_token_secret)
+        #
+        # #api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True, retry_count=10, retry_delay=5, retry_errors=5)
+        # api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+        #
+        # status_id = 1023224248842760194
+        #
+        # tweet = api.get_status(status_id)
+        # actions = []
+        # tweet_id = tweet.id
+        #
+        # action = {
+        #         "_index": "tweet_status_index",
+        #         "_type": "_doc",
+        #         "_id": tweet_id,
+        #         "_source": generateESData(tweet)
+        # }
+        # actions.append(action.copy())
+        #
+        #
+        # user = tweet.user.screen_name
+        # max_id = None
+        #
+        # query = 'to:'+user
+        #
+        # replies = api.search(q=query, since_id=tweet_id, max_id=max_id, count=100)
+        # num = 0
+        # for reply in replies:
+        #     if reply.in_reply_to_status_id == status_id:
+        #
+        #             reply_id = reply.id
+        #             replyAction = {
+        #                     "_index": "tweet_status_index",
+        #                     "_type": "_doc",
+        #                     "_id": reply_id,
+        #                     "_source": generateESData(reply)
+        #             }
+        #
+        #             actions.append(replyAction.copy())
+        #
+        #             print reply.id
+        #             print reply.created_at
+        #             print reply.text
+        #             num = num+1
+        #
+        #         # tweet = get_tweet(reply)
+        #         # client_socket.send((tweet[2] + "\n").encode('utf-8'))
+        # print num
+        #
+        # es = Elasticsearch()
+        # es.indices.create(index="_settings", body=_settings.copy())
+        # res = bulk(es, actions)
+        # es.indices.refresh(index="tweet_status_index")
 
