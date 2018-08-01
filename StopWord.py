d839ac73e29e6e44a702a33cc142ec849af566d0
zaher88abd <zaher88abd@gmail.com>
2018-08-01 11:09:59 -0300

    delete the old code for sparks

--- StopWord.py
+++
@@ -1,7 +0,0 @@
-def load_stop_word():
-    file = "data/scikitlearn_stopword.txt"
-    file = open(file)
-    stop_list = list()
-    for l in file.readlines():
-        stop_list.append(l.split("\n")[0])
-    return stop_list
