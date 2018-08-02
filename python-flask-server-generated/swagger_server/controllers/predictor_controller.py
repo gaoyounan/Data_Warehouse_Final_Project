dfa7745b48b5e679f25f43ca861d93fbeae3edc9
zaher88abd <zaher88abd@gmail.com>
2018-08-02 19:00:33 -0300

    change swagger code

--- python-flask-server-generated/swagger_server/controllers/predictor_controller.py
+++ python-flask-server-generated/swagger_server/controllers/predictor_controller.py
@@ -1,35 +1,8 @@
 import connexion
 import six
-from sklearn.externals import joblib
-from swagger_server.controllers.StringCleaner import cleanText
-import pandas as pd
-import random
-from swagger_server import util
 from swagger_server.controllers.EtractTweets import extractTweets
-
-
-class model_pipline:
-    pipeline_model = None
-
-    def __init__(self) -> None:
-        super().__init__()
-        self.filename = "/home/ubuntu/Data_Warehouse_Final_Project/python-flask-server-generated/pipelineModel/tweet_analyzer.pkl"
-        self.pipeline_model = joblib.load(self.filename)
-
-    def predic_data(self, data_old):
-        data = cleanText(data_old)
-        print(data_old)
-        print(data)
-        for index, row in data.iteritems():
-            if row is None:
-                data[index] = data_old[index]
-        prd_ = self.pipeline_model.predict(data)
-        return prd_
-
-
-def create_model_predict(data):
-    model = model_pipline()
-    return model.predic_data(data)
+from swagger_server.models.byte_array import ByteArray  # noqa: E501
+from swagger_server import util
 
 
 def get_result_tweet_id(itemid):  # noqa: E501
@@ -42,10 +15,21 @@ def get_result_tweet_id(itemid):  # noqa: E501
 
     :rtype: None
     """
-    """
-    this just for testing
-    """
+    return 'do some magic!'
 
+
+def get_result_tweet_id_status(itemid, outputstate):  # noqa: E501
+    """predict the salses
+
+    Returns a single number for the salse # noqa: E501
+
+    :param itemid: ID of item to return
+    :type itemid: int
+    :param outputstate: ID of item to return
+    :type outputstate: int
+
+    :rtype: None
+    """
     extractTweets(itemid, 2, 5)
 
     """
@@ -63,19 +47,33 @@ def get_result_tweet_id(itemid):  # noqa: E501
     return html
 
 
-def set_tweet_id(itemid):  # noqa: E501
+def set_tweet_id(itemid, times, seconds):  # noqa: E501
     """predict the salses
 
     Returns a single number for the salse # noqa: E501
 
     :param itemid: ID of item to return
-    :type itemid: int
+    :type itemid: str
+    :param times: ID of item to return
+    :type times: int
+    :param seconds: ID of item to return
+    :type seconds: int
 
     :rtype: None
     """
+    print(itemid, times, seconds)
+    extractTweets(itemid, times, seconds)
+
+    """
+    End of test
+    """
+
     html = "<html>" \
            "<head><title>Tweet Analyzer</title></head>" \
            "<body><h1>Tweet Analyzer</h1>" \
-           "<h3>Coming Soon</h3></body>" \
+           "<h3>Coming Soon</h3>" \
+           "<h3>"   "</h3>" \
+           "</body>" \
            "</html>"
+
     return html
--- python-flask-server-generated/swagger_server/test/test_predictor_controller.py
+++ python-flask-server-generated/swagger_server/test/test_predictor_controller.py
@@ -5,6 +5,7 @@ from __future__ import absolute_import
 from flask import json
 from six import BytesIO
 
+from swagger_server.models.byte_array import ByteArray  # noqa: E501
 from swagger_server.test import BaseTestCase
 
 
@@ -17,7 +18,18 @@ class TestPredictorController(BaseTestCase):
         predict the salses
         """
         response = self.client.open(
-            '/zaher88abd/DAta/1.0.0/tweetanalys/getresult/{itemid}'.format(itemid=789),
+            '/zaher88abd/DAta/1.0.0/tweetanalys/getresult/{itemid}/'.format(itemid=789),
+            method='GET')
+        self.assert200(response,
+                       'Response body is : ' + response.data.decode('utf-8'))
+
+    def test_get_result_tweet_id_status(self):
+        """Test case for get_result_tweet_id_status
+
+        predict the salses
+        """
+        response = self.client.open(
+            '/zaher88abd/DAta/1.0.0/tweetanalys/getresult/{itemid}/{outputstate}'.format(itemid=789, outputstate=789),
             method='GET')
         self.assert200(response,
                        'Response body is : ' + response.data.decode('utf-8'))
@@ -28,7 +40,7 @@ class TestPredictorController(BaseTestCase):
         predict the salses
         """
         response = self.client.open(
-            '/zaher88abd/DAta/1.0.0/tweetanalys/{itemid}'.format(itemid=789),
+            '/zaher88abd/DAta/1.0.0/tweetanalys/{itemid}/{times}/{seconds}'.format(itemid=ByteArray(), times=789, seconds=789),
             method='GET')
         self.assert200(response,
                        'Response body is : ' + response.data.decode('utf-8'))
